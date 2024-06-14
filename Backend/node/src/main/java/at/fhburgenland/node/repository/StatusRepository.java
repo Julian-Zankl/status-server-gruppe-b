3308599226e0a6b5915989833dafc1d9fdb613cc
NiklasKahrFH <2210859006@fh-burgenland.at>
2024-06-14 10:13:11 +0200

    removed superfluous import statement

--- Backend/node/src/main/java/at/fhburgenland/node/repository/StatusRepository.java
+++ Backend/node/src/main/java/at/fhburgenland/node/repository/StatusRepository.java
@@ -4,7 +4,6 @@ import at.fhburgenland.node.Status;
 import org.springframework.stereotype.Repository;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
