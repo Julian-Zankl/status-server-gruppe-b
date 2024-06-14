22cab0fdcd99a20dcdd62e0bc9a49ff76d3bb36c
Markus Fehringer <2210859004@fh-burgenland.at>
2024-06-14 12:55:45 +0200

    fix update operation

--- Frontend/src/app/routes/update/update.page.ts
+++ Frontend/src/app/routes/update/update.page.ts
@@ -45,7 +45,7 @@ export class StatusUpdatePage {
     const status: StatusDto = {
       username: this.updateStatusForm.value.user ?? '',
       statusText: this.updateStatusForm.value.status ?? '',
-      time: moment().toISOString().slice(0, 19)
+      time: moment().format('YYYY-MM-DDTHH:mm:ss')
     }
 
     firstValueFrom(this.http.put(`/api/statuses/${status.username}`, status)).then(() => {
